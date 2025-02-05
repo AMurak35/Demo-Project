80b9be9006833b76768a074fcfff9b3f090824bf
Anna Murakami <anna.murakami@bequalified.de>
2025-02-05 13:35:37 +0100

    Delete expect

--- tests/inventory-page.spec.ts
+++ tests/inventory-page.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 import { InventoryPage } from '../pages/inventory-page';
 
 test('Add to and remove products from cart on the products page', async ({ page }) => {
